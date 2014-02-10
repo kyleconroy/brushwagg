f8a881c50c2bbb04b901a13060a6c6aa669b44ca
Kyle Conroy <kyle@kyleconroy.com>
2014-02-10 10:33:11 -0800

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -7,6 +7,8 @@ from lxml import etree
 SITE_ROOT = "http://magiccards.info"
 SITE_MAP = "http://magiccards.info/sitemap.html"
 
+GET_CARD_ABILITIES = True
+
 
 def filename_encode(name):
     return ''.join([n for n in name if n in string.digits + string.letters])
@@ -27,7 +29,7 @@ def parse_sets(url):
 
 def parse_cards(url):
     html = etree.HTML(requests.get(url).content)
-    card_set, lang = html.findtext(".//small").split("/")
+    card_set, lang = html.find(".//h1").findtext(".//small").split("/")
     tables = html.findall(".//table")
     table = tables[3]
 
@@ -39,17 +41,36 @@ def parse_cards(url):
                 SITE_ROOT, card_set, lang, no.text)
         image_url = "{}/scans/{}/{}/{}.jpg".format(
                 SITE_ROOT, lang, card_set, no.text)
-        cards.append({
-            "number": int(no.text),
-            "url": card_url,
-            "image": image_url,
-            "name": name.find("a").text,
-            "type": ctype.text,
-            "mana": mana.text,
-            "rarity": rarity.text,
-            "artist": artist.text,
-            "edition": card_set,
-            })
+
+        if GET_CARD_ABILITIES:
+            cardhtml = etree.HTML(requests.get(card_url).content)
+            cardTables = cardhtml.findall(".//table")
+            cardTable = cardTables[3]
+            abilities = cardTable.find(".//b").text
+            cards.append({
+                "number": no.text,
+                "url": card_url,
+                "image": image_url,
+                "name": name.find("a").text,
+                "type": ctype.text,
+                "mana": mana.text,
+                "rarity": rarity.text,
+                "artist": artist.text,
+                "edition": card_set,
+                "abilities": abilities,
+                })
+        else:
+            cards.append({
+                "number": no.text,
+                "url": card_url,
+                "image": image_url,
+                "name": name.find("a").text,
+                "type": ctype.text,
+                "mana": mana.text,
+                "rarity": rarity.text,
+                "artist": artist.text,
+                "edition": card_set,
+                })
 
     return cards
 
@@ -57,7 +78,8 @@ def parse_cards(url):
 if __name__ == "__main__":
 
     # Make sets directory
-    os.mkdir("sets")
+    if not os.path.isdir("sets"):
+        os.mkdir("sets")
 
     for name, url in parse_sets(SITE_MAP):
         card_set = {
@@ -69,3 +91,4 @@ if __name__ == "__main__":
         f = open("sets/{}.json".format(filename_encode(name)), "w")
         json.dump(card_set, f, sort_keys=True, indent=4)
         f.close()
+        print "Finished set"
